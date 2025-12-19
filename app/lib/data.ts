c55becd0f8cf90a907d2d979225610c177d23bed
Dat9161 <155916050+Dat9161@users.noreply.github.com>
2025-12-19 13:16:40 +0700

    Done Chap 8,9&10

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,7 +12,14 @@ import { formatCurrency } from './utils';
 /* ===================== REVENUE ===================== */
 export async function fetchRevenue() {
   try {
-    const { rows } = await pool.query<Revenue>('SELECT * FROM revenue');
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const { rows } = await pool.query<Revenue>(
+      'SELECT * FROM revenue'
+    );
+
+    console.log('Data fetch completed after 3 seconds.');
     return rows;
   } catch (error) {
     console.error('Database Error:', error);
